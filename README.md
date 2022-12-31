f83f7835785175e5b94fe6ff7f0a21d49b927b9c
Julio Mota <juliocesarmotapereira@gmail.com>
2022-12-31 18:58:53 -0300

    mensagens customizadas

--- README.md
+++ README.md
@@ -16,3 +16,4 @@ pratica javascript formularios html5
 - mensagem customizada de erro no balão do navegador;
 - type date;
 - validação de maior idade javascript input.setCustomValidity('Mensagem customizada de erro');
+- input-container--invalido;
--- assets/js/validacao.js
+++ assets/js/validacao.js
@@ -6,32 +6,76 @@
 
 
 export function valida(input) {
-    const tipoDeInput = input.dataset.tipo
+    const tipoDeInput = input.dataset.tipo;
 
     if (validadores[tipoDeInput]) {
-        validadores[tipoDeInput](input)
+        validadores[tipoDeInput](input);
+    }
+
+    if (input.validity.valid) {
+
+        input.parentElement.classList.remove("input-conatainer--invalido");
+        input.parentElement.querySelector(".input-mensagem-erro").innerHTML = "";
+
+    } else {
+        input.parentElement.classList.add("input-container--invalido");
+        input.parentElement.querySelector(".input-mensagem-erro").innerHTML = "mostraMensagemDeErro(tipoDeInput, input)";
     }
 }
 
+// vetor de strings
+const tiposDeErro = [
+    "customError",
+    "patterMismatch",
+    "typeMismatch",
+    "valueMissing",
+]
+
+const mensagemDeErro = {
+
+    nome: {
+        valueMissing: "O campo nome não pode estar vazio."
+    },
+    email: {
+        typeMismatch: "O emai digitado não é válido.",
+        valueMissing: "O campo de e-mail não pode estar vazio."
+    },
+    senha: {
+        patterMismatch: "A senha deve conter somente letras minusculas.",
+        valueMissing: "O campo da senha não pode estar vazio."
+    },
+    dataNascimento: {
+        customError: "Você deve ser maior que 18 anos para se cadastrar.",
+        valueMissing: "O campo data de nascimento não pode estar vazio."
+    }
+}
 
 const validadores = {
     dataNascimento: input => validaDataNascimento(input)
-
 }
 
+function mostraMensagemDeErro(tipoDeInput, input) {
+    let mensagem = "";
+    tiposDeErro.forEach(erro => {
+        if(input.validity[erro]) {
+            mensagem = mensagemDeErro[tipoDeInput][erro];
+        }
+    })
+
+    return mensagem;
+}
 
 function validaDataNascimento(input) {
     const dataRecebida = new Date(input.value);
-    let mensagem = ""
+    let mensagem = "";
 
     maiorQue18(dataRecebida);
 
     if (!maiorQue18(dataRecebida)) {
-        mensagem = "Você deve ser maior que 18 anos para se cadastrar."
-    };
+        mensagem = "Você deve ser maior que 18 anos para se cadastrar.";
+    }
 
     input.setCustomValidity(mensagem);
-
 }
 
 function maiorQue18(data) {
